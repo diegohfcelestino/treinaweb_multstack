2e51e05bda8d7bbaeeee8c7f6f2f766940bacddb
diegohfcelestino <diegohfcelestino@gmail.com>
2021-06-11 20:13:03 -0300

    Primeira aula finalizada, nela contruimos a primeira parte do frontend de forma estatica e estilizamos a tela inicial

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,15 +1,22 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionis de sua localidade'}
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionis de sua localidade"
+        }
+      />
+      <UserInformation
+        name={"Diego Ferreira"}
+        picture={"https://github.com/diegohfcelestino.png"}
+        rating={3}
+        description={"São Paulo"}
       />
     </div>
   );
