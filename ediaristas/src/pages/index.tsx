3aee3958281a8301d8bcbb45d0084e53d7679af3
diegohfcelestino <diegohfcelestino@gmail.com>
2021-06-12 00:29:17 -0300

    Finalizado apagina inicial do frontend

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfessionalsPaper,
+  ProfessionalsContainer,
+} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,12 +19,72 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionis de sua localidade"
         }
       />
-      <UserInformation
-        name={"Diego Ferreira"}
-        picture={"https://github.com/diegohfcelestino.png"}
-        rating={3}
-        description={"São Paulo"}
-      />
+
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>Cep inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfessionalsPaper>
+          <ProfessionalsContainer>
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Diego Ferreira"}
+              picture={"https://github.com/diegohfcelestino.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+          </ProfessionalsContainer>
+        </ProfessionalsPaper>
+      </Container>
     </div>
   );
 }
